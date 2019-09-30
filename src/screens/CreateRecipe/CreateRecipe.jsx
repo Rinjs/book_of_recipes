c703100f3a3d00926e28699386e90fd80876c73b
Vladislav Korenik <vladyha66551@gmail.com>
2019-09-30 18:11:32 +0300

    fixed styles bug and added style for create recipe page

--- src/screens/CreateRecipe/CreateRecipe.jsx
+++ src/screens/CreateRecipe/CreateRecipe.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./CreateRecipe.css";
 
-const CreateRecipe = () => (
-  <Link to="/">back</Link>
-);
+const CreateRecipe = () => {
+  const [inputArr, setNewInput] = useState([
+    <input placeholder="1." key={Math.random()} />
+  ]);
+  const AddNewPlace = () => {
+    const arr = [
+      ...inputArr,
+      <input placeholder={`${inputArr.length + 1}.`} key={Math.random()} />
+    ];
+    setNewInput(arr);
+  };
+  return (
+    <div className="create-recipe">
+      <form action="" className="create-recipe-form">
+        <input placeholder="Enter dish name" />
+        <div className="create-recipe-form-ingredient-list">
+          <p>Enter ingredients:</p>
+          {inputArr}
+          <button onClick={() => AddNewPlace()} type="button">
+            <i className="fas fa-plus"></i> New ingredient
+          </button>
+        </div>
+        <textarea placeholder="Enter how cook this dish" />
+      </form>
+      <Link to="/">
+        <i className="fas fa-backward"></i> back
+      </Link>
+    </div>
+  );
+};
 
 export default CreateRecipe;
